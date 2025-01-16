74fc1222e42ce80040aade943fe0cfe218eebafd
Kausar Khan <kausarrkhan83@gmail.com>
2025-01-16 15:39:53 +0530

    Add new component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import MyComponent from "./MyComponent";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <div id="root">
+        <MyComponent />
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/MyComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyComponent = () => {
+  return <p>I am learning React. My life is getting better.</p>;
+};
+
+export default MyComponent;
